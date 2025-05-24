1996d220156da462ed191c9adad722456d207b89
MatejMa2ur <matejmazur@outlook.com>
2025-05-24 19:45:36 +0200

    feat: upgrade dependencies and enhance map functionality
    - Upgrade Aspire AppHost SDK and dependencies to version 9.3.0
    - Add support for Docker packaging with container image tags and repositories for LayerHub.Api and LayerHub.Web
    - Enhance map functionality:
      - Add user authentication state awareness (`_isAuthenticated`)
      - Implement dynamic map feature loading for authenticated users
      - Add random globe points and utilize globe projection for unauthenticated users
      - Adjust map controls and zoom behavior dynamically
      - Improve map bounds calculation and error handling
    - Update LayerHub_Web and LayerHub_Api project references and settings

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -6,26 +6,27 @@ var builder = DistributedApplication.CreateBuilder(args);
 var username = builder.AddParameter("username", secret: true);
 var password = builder.AddParameter("password", secret: true);
 
-var postgres = builder.AddPostgres("LayerHub", username, password, port: 5433)
+var postgres = builder.AddPostgres("LayerHubPostgreSql", username, password, port: 5433)
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 var postgresdb = postgres.AddDatabase("LayerHubWriteDb");
 
 // Setup MongoDB
-var mongo = builder.AddMongoDB("LayerHub-Mongo")
+var mongo = builder.AddMongoDB("LayerHubMongo")
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 var mongodb = mongo.AddDatabase("LayerHubReadDb");
 
 // Setup API
-var api = builder.AddProject<LayerHub_Api>("Api", launchProfileName: "https")
+var api = builder.AddProject<LayerHub_Api>("Api")
     .WithReference(mongodb, "MongoDbConnection")
     .WithReference(postgresdb, "DefaultConnection")
     .WaitFor(mongodb)
     .WaitFor(postgresdb);
 
 // Setup Web
-builder.AddProject<LayerHub_Web>("Web", launchProfileName: "https")
+builder.AddProject<LayerHub_Web>("Web")
+    .WithExternalHttpEndpoints()
     .WithReference(api);
 
 builder.Build().Run();
